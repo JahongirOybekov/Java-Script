a72ffb32cb2363ac76e9d6a30031bf9e87e5ab6c
JahongirOybekov <oybekov6400@gmail.com>
2026-06-22 16:26:13 +0500

    add

---
+++ main.js
@@ -0,0 +1,258 @@
+// Forsickli
+// ++++++===============
+
+// for (let boshlanishi = 0; boshlanishi <= 10; boshlanishi++) {
+//     console.log(boshlanishi)
+// }
+
+
+// for (let boshi = 3; boshi <= 23; boshi++) {
+//     console.log(boshi)
+// }
+// ============
+
+// for (let i = 0; i <101; i++){
+//     if (i % 2 === 0)
+//         console.log(i)
+// }
+
+// for (let i = 0; i <101; i++){
+//     if (i % 2 ! === 0) {
+//         console.log (i)
+//     }
+// }
+
+// for (let b = 0; b <= 100; b++){
+//     if ( b % 5 == 0 && b % 3 == 0 ){
+//         console.log(b)
+//     }
+// }
+
+// 1-topshiriq
+
+// for (let i = 0; i <= 1000; i++) {
+//     let mingacha = i * i;
+//     console.log( + mingacha);
+// }
+
+// 2-topshiriq
+
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % 3 === 0 && i % 4 === 0) {
+//         let kubi = i ** 3;
+//         console.log( + kubi);
+//     }
+// }
+
+// 3-topshiriq
+
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % 5 === 0 && i % 2 === 0) {
+//         console.log( + i);
+//     }
+// }
+
+
+// let i = 1
+// while (i <= 5){
+//     console.log(i)
+//     i++
+// }
+
+// let i = 1
+// let opshi = 0
+// while (i <= 20) {
+//     console.log(i)
+//     opshi += i
+//     i++
+// }
+// console.log(opshi)
+// console.log(`1dan 20 gacha bo'lgan sonlarning yig'indisi ${opshi}ga teng`)
+
+
+// while
+
+
+
+// let n = 1
+// while (n < 100){
+//     n *= 2
+// }
+// console.log(n)
+
+// let i = 1
+// while (i < 1000){
+//     i *= 3
+// }
+// console.log(i)
+
+// do
+
+// let i = 0;
+
+// do {
+//     let kvadrat = i ** 2;
+//     console.log(`${kvadrat}`);
+//     i += 2;
+// } while (i <= 100);
+
+
+// let i = 1; 
+
+// do {
+//     let kub = i ** 3;
+//     console.log(`${kub}`);
+//     i += 2; 
+// } while (i <= 100);
+
+// 1
+
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+//  2
+
+// let i = 5;
+
+// do {
+//     console.log(i);
+//     i--;
+// } while (i >= 1);
+
+// 3
+
+// let i = 0;
+
+// do {
+//   console.log("Hello");
+// } while (--i > 0);
+
+// let b = 0;
+
+// do {
+//   console.log("Hello");
+// } while (--b > 0);
+
+// let c = 0;
+
+// do {
+//   console.log("Hello");
+// } while (--c > 0);
+
+// 4
+
+// let son;
+// do {
+//     son = prompt("Son kiriting:");
+// } while (son != 7);
+
+// 5
+
+// let i = 2;
+
+// do {
+//     console.log(i);
+//     i += 2;
+// } while (i <= 10);
+
+// 6
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i += 2;
+// } while (i <= 10);
+
+// 7
+
+// let son = 3;
+// let i = 0;
+// let natija = "";
+
+// while (i < 5) {
+//     natija += son + " ";
+//     i++;
+// }
+// console.log(natija.trim());
+
+// 8
+
+// let Matn;
+
+// do {
+//     Matn = prompt("Matn kiriting:");
+// } while (Matn.toLowerCase() !== "stop");
+
+// console.log("To'g'ri")
+
+// 9
+
+
+
+
+
+
+
+// modullar
+
+// let names = ["Saidjon",
+//     "Azamat",
+//     "Suxrob",
+//     "Jahongir",
+//     "Shohjahon",
+//     "Baxrom",
+//     "Asadbek",
+//     "Tursunboy",
+//     "Aminboy",
+//     "Bekzod",
+//     "Kamronbek",
+//     "Kamoladdin"]
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
+
+// for(let ozgaruvchi of names){
+//     console.log(ozgaruvchi)
+// }
+
+// console.log(names[0])
+// console.log(names[2])
+// console.log(names[4])
+// console.log(names[3])
+
+// let massiv = new Array(4)
+// let massiv0 = new Array(4, 1, 2, 3, 5)
+// let massiv1 = []
+// console.log(massiv)
+// console.log(massiv0)
+// console.log(massiv1)
+// console.log(names.length)
+
+// Array methods
+// let pushMethod = names.push("oxirga qo'shiladi")
+// console.log(names)
+
+
+// let popMethod = names.pop("")
+// console.log(names)
+// console.log(popMethod)
+
+// let unshiftMethod = names.unshift("Boshiga qo'shiladi")
+// console.log(names)
+
+// let shiftMethod = names.shift()
+// console.log(names)
+// console.log(shiftMethod) 
+
+// let massiv = ;
+
+// for (let i = 0; i < massiv.length; i++) {
+//     console.log(massiv[i]);
+// }
+
