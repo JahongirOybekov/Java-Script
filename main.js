fc48051a2787c7b991fa2fd603b6cdc864caa8d9
JahongirOybekov <oybekov6400@gmail.com>
2026-06-29 16:55:34 +0500

    add

--- main.js
+++ main.js
@@ -256,3 +256,141 @@
 //     console.log(massiv[i]);
 // }
 
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while (i <= 20) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// let son;
+
+// while (true) {
+//     son = Number(prompt("Son kiriting (to'xtatish uchun 0 kiriting):"));
+//     if (son === 0) {
+//         console.log("To'g'ri");
+//         break;
+//     }
+//     console.log("Siz kiritgan son: " + son);
+// }
+
+// let arr = ["Shohruz", 12, 46, false, true, undefined, Boolean, Number, String]
+// for (let i of arr) {
+//     console.log(i)
+// }
+
+// let arr = [-1, 1, 3, -12, 6, 4, -22, 6]
+// let musbat = []
+// for (let i of arr) {
+//     if (i > 0)
+//         musbat.push(i)
+// }
+// console.log(musbat.length)
+
+// let yigindi = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     yigindi += i;
+// }
+
+// console.log( yigindi);
+
+
+// let son = 457;
+// let teskariSon = 0;
+
+// while (son > 0) {
+//     let oxirgiRaqam = son % 10;         
+//     teskariSon = (teskariSon * 10) + oxirgiRaqam; 
+//     son = Math.floor(son / 10);   
+// }
+
+// console.log("Teskari tartibda:", teskariSon); 
+
+
+// const toqSonlar = []
+// let yigindi = 0;
+
+// for (let i = 1; i <= 50; i += 2) {
+//     toqSonlar.push(i);
+// }
+
+// for (let a = 0; a < toqSonlar.length; a++) {
+//     yigindi += toqSonlar[a];
+// }
+
+// console.log("1dan 50gacha bo'lgan toq sonlarni yig'indisi:", yigindi);
+
+
+// const massiv =[1,2,3,4,5];
+
+// for (let i = massiv.length - 1; i >= 0; i--) {
+//     console.log(massiv[i]);
+// }
+
+// let son = parseInt(prompt("Son kiriting:")); 
+
+// let yigindi = 0;
+
+// if (!isNaN(son)) {
+
+//     son = Math.abs(son);
+
+//     while (son > 0) {
+//         let qoldiq = son % 10;   
+//         yigindi += qoldiq;      
+//         son = Math.floor(son / 10); 
+//     }
+
+//     console.log("Kiritilgan son raqamlari yig'indisi:", yigindi);
+// } else {
+//     console.log("Iltimos, faqat son kiriting!");
+// }
+
+// let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54];
+// arr.pop()
+// arr.push(1,2,3,4,5)
+// arr.unshift(1,2,3,4,5)
+// arr.shift()
+// arr.splice(1, 15, 7);
+// arr.splice(1, 15);
+// arr.splice(1);
+// let indeksiniQaytarish = arr.indexOf(54)
+// let indeksiniQaytarish1 = arr.indexOf(545)
+
+
+// console.log(indeksiniQaytarish, indeksiniQaytarish1)
+// let massiv = arr.lastIndexOf(54)
+// let findMetod = arr.find(i => i > 300)
+// console.log(findMetod)
+
+
+
+// 14
+// const massiv = [4, -5, 0, -2, 11, -8, 7];
+
+// let manfiySoni = 0;
+
+// for (let i = 0; i < massiv.length; i++) {
+//     if (massiv[i] < 0) {
+//         manfiySoni++;
+//     }
+// }
+
+// console.log("Massivdagi manfiy sonlar soni:", manfiySoni);
+
+
+// 15
+// for (let i = 1; i <= 15; i++) {
+//     let kvadrat = i * i;
+//     console.log(` ${kvadrat}`);
+// }
+
+
+// 16
