32c98e4c873b4adf8c2628a506964840b3852ce1
JahongirOybekov <oybekov6400@gmail.com>
2026-07-08 16:36:29 +0500

    add

--- masalalar.js
+++ masalalar.js
@@ -10,10 +10,19 @@
 // 2-topshiriq (push)
 // Bo'sh "savat" massivi yarating. Foydalanuvchi tanlagan 3 ta mahsulot nomini (o'zingiz kiriting)
 // push() yordamida qo'shing va oxirida savatdagi mahsulotlar sonini chop eting.
-let mahsulotlar = []
-let mahsulot;
-while (mahsulotlar.length < 3) {
-   mahsulot = prompt("Mahsulot nomini kiriting!: ")
-   mahsulotlar.push(mahsulot)
-}
-console.log(mahsulotlar)
+// let mahsulotlar = []
+// let mahsulot;
+// while (mahsulotlar.length < 3) {
+//    mahsulot = prompt("Mahsulot nomini kiriting!: ")
+//    mahsulotlar.push(mahsulot)
+// }
+// console.log(mahsulotlar)
+
+// 11-topshiriq (forEach)
+// let narxlar = [1000, 2500, 3200, 4700];
+// forEach() yordamida har bir narxga 15% QQS qo'shib, natijani "Narx: XXXX so'm" formatida chop eting
+// (asl massivni o'zgartirmasdan).
+let narxlar = [1000, 2500, 3200, 4700];
+narxlar.forEach((son, i) => {
+   console.log(`${i}-indeksdagi mahsulot narxi ${son*15/100+son} so'm`)
+})
