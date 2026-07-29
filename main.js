70199c98d8e5dbc658f84f7ae7316897173facab
JahongirOybekov <oybekov6400@gmail.com>
2026-07-29 16:24:01 +0500

    add

--- main.js
+++ main.js
@@ -543,3 +543,41 @@ const shoxruzbek = {
 // }
 
 
+function userData() {
+  let ism = prompt("Ismingizni kiriting: ")
+  let familiya = prompt("Familiyangizni kiriting: ")
+  let yosh = Number(prompt("Yoshingizni kiriting: "))
+
+  let malumoti = `Sizning ismingiz ${ism}, familiyangiz ${familiya} va yoshingiz ${yosh}da!`
+  
+  console.log(malumoti)
+}
+
+// userData()
+
+// _______________________________________________________
+
+function daraja(a) {
+  return a ** 2
+}
+
+// console.log(daraja(2))
+
+// _______________________________________________________
+function darajaa(b) {
+  console.log("salom")
+  return b
+  
+}
+
+// darajaa(2)
+// console.log(darajaa(2))
+
+// _______________________________________________________
+
+function qoshish(a, b) {
+  return a + b;
+}
+console.log(qoshish(5, 8))
+console.log(qoshish(5,8,4))
+console.log(qoshish(5)) //! NaN - Not a Number
