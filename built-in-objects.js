f80fe450acb004b8dc82da61035162be801bfef1
JahongirOybekov <oybekov6400@gmail.com>
2026-08-05 16:52:26 +0500

    add

--- built-in-objects.js
+++ built-in-objects.js
@@ -28,7 +28,9 @@
 
 //^ 5-topshiriq Hozirgi sanani ISO formatda (YYYY-MM-DDTHH:mm:ss.sssZ) konsolga chiqaruvchi dastur yozing.
 
-
+const hozirgiVaqt = new Date();
+const isoFormat = hozirgiVaqt.toISOString();
+console.log("ISO formatdagi sana va vaqt:", isoFormat);
 
 //^ 6-topshiriq 1970-yil 1-yanvardan hozirgi vaqtgacha bo'lgan millisekundlar sonini konsolga chiqaruvchi dastur yozing.
 
