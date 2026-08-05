59acf5f52a7553bc3d5abe370f0bb84ba9ccb23e
JahongirOybekov <oybekov6400@gmail.com>
2026-08-05 17:04:57 +0500

    add

--- built-in-objects.js
+++ built-in-objects.js
@@ -51,12 +51,16 @@
 
 //^ 8-topshiriq Hozirgi vaqtga 5 soat qo'shib, yangi vaqtni konsolga chiqaruvchi dastur yozing.
 
-const vaqt = new Date();
-vaqt.setHours(vaqt.getHours() + 5);
-console.log(vaqt.toString());
+// const vaqt = new Date();
+// vaqt.setHours(vaqt.getHours() + 5);
+// console.log(vaqt.toString());
 
 //^ Math Object (9-19 topshiriqlar)
+// ===============================================================================
+
 //^ 9-topshiriq Math.PI yordamida Pi sonini konsolga chiqaring.
+
+
 
 //^ 10-topshiriq 0 va 1 oralig'ida tasodifiy son hosil qiling va konsolga chiqaring.
 
