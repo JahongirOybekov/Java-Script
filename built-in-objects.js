e8f93883db7ab0c9871846edad0e9d0a088b9447
JahongirOybekov <oybekov6400@gmail.com>
2026-08-05 16:58:11 +0500

    add

--- built-in-objects.js
+++ built-in-objects.js
@@ -51,6 +51,8 @@
 
 //^ 8-topshiriq Hozirgi vaqtga 5 soat qo'shib, yangi vaqtni konsolga chiqaruvchi dastur yozing.
 
+
+
 //^ Math Object (9-19 topshiriqlar)
 //^ 9-topshiriq Math.PI yordamida Pi sonini konsolga chiqaring.
 
