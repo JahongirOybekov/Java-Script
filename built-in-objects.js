5b1a4557332340d611411c4ecfa1e2169a443aa3
JahongirOybekov <oybekov6400@gmail.com>
2026-08-05 17:40:09 +0500

    add

--- built-in-objects.js
+++ built-in-objects.js
@@ -94,6 +94,8 @@
 
 //^ 15-topshiriq [5, 10, 3, 8] massividagi eng kichik sonni toping va konsolga chiqaring.
 
+
+
 //^ 16-topshiriq -7 sonining absolut (modul) qiymatini toping va konsolga chiqaring.
 
 //^ 17-topshiriq 2 ning 3-darajasini hisoblang va konsolga chiqaring.
