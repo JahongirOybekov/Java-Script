6db24699f4c00dc5fc56db09ef368efe760158ff
JahongirOybekov <oybekov6400@gmail.com>
2026-08-07 17:15:47 +0500

    add

--- mainn.js
+++ mainn.js
@@ -2,3 +2,6 @@ const box = document.getElementById("box");
 // console.log(box)
 
 box.style.backgroundColor = "blue";
+box.style.width = "500px";
+box.style.height = "500px";
+box.style.borderRadius = "50%";
