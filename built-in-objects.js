a6d90252185eef8a1b249b108d584a7321bcff0c
JahongirOybekov <oybekov6400@gmail.com>
2026-08-07 16:26:58 +0500

    add

--- built-in-objects.js
+++ built-in-objects.js
@@ -150,10 +150,10 @@
 
 //^ 24-topshiriq "3.14" matnini Number.parseFloat() yordamida haqiqiy songa o'giring va konsolga chiqaring.
 
-let matn = "3.14";
-let son = Number.parseFloat(matn);
-console.log(son); 
-console.log(typeof son);
+// let matn = "3.14";
+// let son = Number.parseFloat(matn);
+// console.log(son); 
+// console.log(typeof son);
 
 //^ JSON Object (25-27 topshiriqlar)
 //^ 25-topshiriq {name: "Ali", age: 25} ob'ektini JSON string formatiga o'giring va konsolga chiqaring.
