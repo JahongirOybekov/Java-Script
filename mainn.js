4d2919587f989061f2bf9413bfdd3944fa1e8e89
JahongirOybekov <oybekov6400@gmail.com>
2026-08-07 17:41:02 +0500

    add

--- mainn.js
+++ mainn.js
@@ -1,7 +1,14 @@
 const box = document.getElementById("box");
 // console.log(box)
 
-// box.style.backgroundColor = "blue";
-// box.style.width = "500px";
-// box.style.height = "500px";
-// box.style.borderRadius = "50%";
+box.style.border = "2px solid black"
+box.style.width = "1500px";
+box.style.height = "600px";
+box.style.margin = "auto"
+box.style.marginTop = "50px"
+
+const box1 = document.getElementById("box1");
+
+box1.style.width = "200px"
+box1.style.height = "150px"
+box1.style.backgroundColor = "yellow"
