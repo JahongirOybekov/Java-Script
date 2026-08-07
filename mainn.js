a05d9643b866a611520eaa3cf6e8ab67633c95b8
JahongirOybekov <oybekov6400@gmail.com>
2026-08-07 17:45:37 +0500

    add

--- mainn.js
+++ mainn.js
@@ -12,3 +12,22 @@ const box1 = document.getElementById("box1");
 box1.style.width = "200px"
 box1.style.height = "150px"
 box1.style.backgroundColor = "yellow"
+
+const box2 = document.getElementById("box2");
+
+box2.style.width = "200px"
+box2.style.height = "150px"
+box2.style.backgroundColor = "blue"
+
+const box3 = document.getElementById("box3");
+
+box3.style.width = "200px"
+box3.style.height = "150px"
+box3.style.backgroundColor = "red"
+
+const box4 = document.getElementById("box4");
+
+box4.style.width = "200px"
+box4.style.height = "150px"
+box4.style.backgroundColor = "green"
+
