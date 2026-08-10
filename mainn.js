7ce6bbe94e6c65999aa2b3c6542513349fc95217
JahongirOybekov <oybekov6400@gmail.com>
2026-08-10 16:55:12 +0500

    add

--- mainn.js
+++ mainn.js
@@ -1,50 +1,50 @@
-const box = document.getElementById("box");
-// console.log(box)
+// const box = document.getElementById("box");
+// // console.log(box)
 
-box.style.border = "2px solid black"
-box.style.width = "1500px";
-box.style.height = "600px";
-box.style.margin = "auto"
-box.style.marginTop = "50px"
-box.style.display = "flex"
-box.style.justifyContent = "space-around"
-box.style.alignItems = "center"
+// box.style.border = "2px solid black"
+// box.style.width = "1500px";
+// box.style.height = "600px";
+// box.style.margin = "auto"
+// box.style.marginTop = "50px"
+// box.style.display = "flex"
+// box.style.justifyContent = "space-around"
+// box.style.alignItems = "center"
 
-const box1 = document.getElementById("box1");
+// const box1 = document.getElementById("box1");
 
-box1.style.width = "200px"
-box1.style.height = "150px"
-box1.style.backgroundColor = "yellow"
+// box1.style.width = "200px"
+// box1.style.height = "150px"
+// box1.style.backgroundColor = "yellow"
 
-const box2 = document.getElementById("box2");
+// const box2 = document.getElementById("box2");
 
-box2.style.width = "200px"
-box2.style.height = "150px"
-box2.style.backgroundColor = "blue"
+// box2.style.width = "200px"
+// box2.style.height = "150px"
+// box2.style.backgroundColor = "blue"
 
-const box3 = document.getElementById("box3");
+// const box3 = document.getElementById("box3");
 
-box3.style.width = "200px"
-box3.style.height = "150px"
-box3.style.backgroundColor = "red"
+// box3.style.width = "200px"
+// box3.style.height = "150px"
+// box3.style.backgroundColor = "red"
 
-const box4 = document.getElementById("box4");
+// const box4 = document.getElementById("box4");
 
-box4.style.width = "200px"
-box4.style.height = "150px"
-box4.style.backgroundColor = "green"
+// box4.style.width = "200px"
+// box4.style.height = "150px"
+// box4.style.backgroundColor = "green"
 
-const box5 = document.getElementById("box5");
+// const box5 = document.getElementById("box5");
 
-box5.style.width = "200px"
-box5.style.height = "150px"
-box5.style.backgroundColor = "yellowgreen"
+// box5.style.width = "200px"
+// box5.style.height = "150px"
+// box5.style.backgroundColor = "yellowgreen"
 
-const box6 = document.getElementById("box6");
+// const box6 = document.getElementById("box6");
 
-box6.style.width = "200px"
-box6.style.height = "150px"
-box6.style.backgroundColor = "pink"
+// box6.style.width = "200px"
+// box6.style.height = "150px"
+// box6.style.backgroundColor = "pink"
 
 
 
