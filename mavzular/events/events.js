26295afba1809d68bb1a7f90b07a7990407379c6
JahongirOybekov <oybekov6400@gmail.com>
2026-08-12 17:01:03 +0500

    add

--- mavzular/events/events.js
+++ mavzular/events/events.js
@@ -1,16 +1,16 @@
-const div1 = document.getElementById("div1");
-const div2 = document.getElementById("div2");
+// const div1 = document.getElementById("div1");
+// const div2 = document.getElementById("div2");
 
-div1.addEventListener("click", () => {
-  console.log("Sichqonchani chap tarafini bir marta bosdingiz!");
-  div1.style.backgroundColor = "red";
-  div1.style.width = "600px"
-  div1.style.height = "300px"
-  div1.style.transition = "1s"
-  div1.innerHTML = `<button style="width: 100px; height: 60px; border: none; background-color: green; color: snow;">Send</button>`
+// div1.addEventListener("click", () => {
+//   console.log("Sichqonchani chap tarafini bir marta bosdingiz!");
+//   div1.style.backgroundColor = "red";
+//   div1.style.width = "600px"
+//   div1.style.height = "300px"
+//   div1.style.transition = "1s"
+//   div1.innerHTML = `<button style="width: 100px; height: 60px; border: none; background-color: green; color: snow;">Send</button>`
   
-});
+// });
 
-div2.addEventListener("dblclick", () => {
-  console.log("Sichqonchani chap tarafini ikki marta bosdingiz!");
-});
+// div2.addEventListener("dblclick", () => {
+//   console.log("Sichqonchani chap tarafini ikki marta bosdingiz!");
+// });
