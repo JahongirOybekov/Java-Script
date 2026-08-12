0512dd65e8512c8bed779b0407574ace47b72f82
JahongirOybekov <oybekov6400@gmail.com>
2026-08-12 16:52:45 +0500

    add

--- mavzular/events/events.js
+++ mavzular/events/events.js
@@ -3,6 +3,7 @@ const div2 = document.getElementById("div2");
 
 div1.addEventListener("click", () => {
   console.log("Sichqonchani chap tarafini bir marta bosdingiz!");
+  div1.style.backgroundColor = "red";
 });
 
 div2.addEventListener("dblclick", () => {
