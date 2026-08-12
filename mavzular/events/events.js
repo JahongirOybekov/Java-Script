76387ff6118587a67fa156e10210e0ab95f63ffd
JahongirOybekov <oybekov6400@gmail.com>
2026-08-12 16:48:31 +0500

    add

---
+++ mavzular/events/events.js
@@ -0,0 +1,10 @@
+const div1 = document.getElementById("div1");
+const div2 = document.getElementById("div2");
+
+div1.addEventListener("click", () => {
+  console.log("Sichqonchani chap tarafini bir marta bosdingiz!");
+});
+
+div2.addEventListener("dblclick", () => {
+  console.log("Sichqonchani chap tarafini ikki marta bosdingiz!");
+});
