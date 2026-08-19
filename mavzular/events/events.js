268924201e7b0568741fbbc07323a9d44cca3c38
JahongirOybekov <oybekov6400@gmail.com>
2026-08-19 17:18:23 +0500

    add

--- mavzular/events/events.js
+++ mavzular/events/events.js
@@ -139,3 +139,41 @@ const input = document.querySelector("#todoInput")
 const addBtn = document.querySelector("#addBtn")
 const todoList = document.querySelector("todoList")
 const count = document.querySelector("#count")
+
+addBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+    const text = input.value.trim();
+
+    if (text === "") {
+        alert("Siz inputga qiymat yozmadingiz!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+                <span>${text}</span>
+                <button class="delete-btn">O'chirish</button>
+            `;
+
+    todoList.appendChild(li);
+
+    input.value = "";
+
+    updateCount();
+
+    li.querySelector("span").addEventListener("click", () => {
+        li.classList.toggle("completed")
+    })
+
+    li.querySelector(".delete-btn").addEventListener("click", () => {
+        li.remove()
+        updateCount()
+    })
+
+
+
+    function updateCount() {
+        count.textContent = todoList.children.length;
+    }
+}
